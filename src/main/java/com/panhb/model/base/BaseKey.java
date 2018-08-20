eda36fadab982dbb3cfef06086bfc3d1d39d6282
hongbo.pan <hongbo.pan@zhaogang.china.com>
2018-08-20 09:32:15 +0800

    feat: pom应用修改

--- src/main/java/com/panhb/model/base/BaseKey.java
+++ src/main/java/com/panhb/model/base/BaseKey.java
@@ -16,24 +16,22 @@ public class BaseKey {
     private String key4;
     private String key5;
 
+
     @Override
     public boolean equals(Object o) {
-        if (this == o){
-            return true;
-        }
-        if (!(o instanceof BaseKey)) {
-            return false;
-        }
+        if (this == o) return true;
+        if (!(o instanceof BaseKey)) return false;
         BaseKey baseKey = (BaseKey) o;
-        return Objects.equal(getKey1(), baseKey.getKey1()) &&
-                Objects.equal(getKey2(), baseKey.getKey2()) &&
-                Objects.equal(getKey3(), baseKey.getKey3()) &&
-                Objects.equal(getKey4(), baseKey.getKey4()) &&
-                Objects.equal(getKey5(), baseKey.getKey5());
+        return java.util.Objects.equals(getKey1(), baseKey.getKey1()) &&
+                java.util.Objects.equals(getKey2(), baseKey.getKey2()) &&
+                java.util.Objects.equals(getKey3(), baseKey.getKey3()) &&
+                java.util.Objects.equals(getKey4(), baseKey.getKey4()) &&
+                java.util.Objects.equals(getKey5(), baseKey.getKey5());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hashCode(getKey1(), getKey2(), getKey3(), getKey4(), getKey5());
+
+        return java.util.Objects.hash(getKey1(), getKey2(), getKey3(), getKey4(), getKey5());
     }
 }
